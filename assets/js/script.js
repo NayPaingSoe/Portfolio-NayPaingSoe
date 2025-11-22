bcbbac9b92f11f309192c8dee46fa8631a3491a0
NayPaingSoe <naypaingsoe042@gmail.com>
2025-11-22 19:29:33 +0630

    feat: update skills section with new icons and remove dynamic fetching

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,34 +88,6 @@ var typed = new Typed(".typing-text", {
 });
 // <!-- typed js effect ends -->
 
-async function fetchData(type = "skills") {
-  let response;
-  type === "skills"
-    ? (response = await fetch("skills.json"))
-    : (response = await fetch("./projects/projects.json"));
-  const data = await response.json();
-  return data;
-}
-
-function showSkills(skills) {
-  let skillsContainer = document.getElementById("skillsContainer");
-  let skillHTML = "";
-  skills.forEach((skill) => {
-    skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src="${skill.icon}" alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`;
-  });
-  skillsContainer.innerHTML = skillHTML;
-}
-
-fetchData().then((data) => {
-  showSkills(data);
-});
-
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
   max: 15,
